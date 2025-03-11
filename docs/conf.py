ce332dbfcd67101b732cd7fe9776e8de600d6310
Yana Hontyk <yana.hontyk@canonical.com>
2025-03-11 11:59:31 +0100

    fix(conf.py): enable feedback button

--- docs/conf.py
+++ docs/conf.py
@@ -145,6 +145,9 @@ html_context = {
     # TODO: To enable or disable the Previous / Next buttons at the bottom of pages
     # Valid options: none, prev, next, both
     "sequential_nav": "both",
+
+    # Required for feedback button    
+    'github_issues': 'enabled',
 }
 
 # Project slug; see https://meta.discourse.org/t/what-is-category-slug/87897
