755b98b8a3a30e896eb390bba5c837d077aeacd6
Michael Park <michael.park@canonical.com>
2025-03-21 15:01:39 +1000

    fix: 404 url fix
    Signed-off-by: Michael Park <michael.park@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -155,7 +155,7 @@ html_context = {
 # TODO: If your documentation is hosted on https://docs.ubuntu.com/,
 #       uncomment and update as needed.
 
-# slug = ''
+slug = 'chisel'
 
 
 # Template and asset locations
